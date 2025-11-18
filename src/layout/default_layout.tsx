2a4d831e93dc5f515dc53bf311198dba64fb71a9
Joseph_Ortiz <utm24090745@utma.edu.mx>
2025-11-18 12:32:17 -0600

    Proyecto 18/11/2025

---
+++ src/layout/default_layout.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+export default function DefaultLayout(){
+    <>
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+}
