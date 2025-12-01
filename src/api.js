23f4001a50580973c88c96c802446cf3fb1ee6ac
unknown <utm24090620@utma.edu.mx>
2025-12-01 12:33:33 -0600

    Avance Abi

---
+++ src/api.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function MyComponent() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://backend-proyect2-2.onrender.com')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data.map(item => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default MyComponent;
